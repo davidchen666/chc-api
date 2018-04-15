db4c93dee3c6792b4bae378045091ce47123604c
davidchen <davidchen106106@gmail.com>
2018-04-15 23:01:32 +0800

    后台管理系统登录

--- controller/controller.index.php
+++ controller/controller.index.php
@@ -15,50 +15,12 @@ class IndexController extends Controller
     function __construct()
     {
         $this->model = Model::instance(self::M);
-        $this->title='首页';
     }
 
-    /**
-     * 首页
-     */
-    function index()
-    {
-        // $name = "张三";
-        // $data = array(
-        //     'name'=>$name,
-        //     'age'=>22,
-        //     'addr'=>"上海"
-        //     );
-        $data['title'] = $this->title;
-        // $getData = $this->model->index(5);
-        // var_dump($getData);
-        View::instance('index/index.tpl')->show($data);
-        // echo to_success($getData);
+    function verifyLogin(){
+        echo $this->model->verifyLogin();
     }
 
-    function test2()
-    {
-        View::instance('index/test2.tpl')->show(array());
-    }
-
-    function uploadFile()
-    {
-        $targetFolder = 'iresearch_ui/public/uploads';
-        if (!empty($_FILES)) {
-            $tempFile = $_FILES['Filedata']['tmp_name'];
-            $targetPath = $_SERVER['DOCUMENT_ROOT'] . $targetFolder;
-            $targetFile = rtrim($targetPath, '/') . '/' . $_FILES['Filedata']['name'];
-            echo $targetFile;
-            $fileTypes = array('xls', 'xlsx', 'jpg', 'jpeg', 'avi', 'mp4', 'rar', 'doc', 'docx', 'png');
-            $fileParts = pathinfo($_FILES['Filedata']['name']);
-            if (in_array($fileParts['extension'], $fileTypes)) {
-                move_uploaded_file($tempFile, $targetFile);
-                echo '上传成功';
-            } else {
-                echo '文件类型错误';
-            }
-        }
-    }
 }
 
 ?>
--- model/model.index.php
+++ model/model.index.php
@@ -2,10 +2,22 @@
 
 class IndexModel extends AgentModel
 {
-    public function index($data){
-        $sql = "select * from chc_site_page where id={$data}";
-        $res = $this->mysqlQuery($sql, "all");
-        return $res;
+    public function verifyLogin(){
+        $pData = getData();
+        // $res = $this->mysqlQuery($sql, "all");
+        if($pData['username'] && $pData['password']){
+        	$newPwd = md5(md5($pData['password'].'chc'));
+        	$sqltotal = "select id userid,user_name username FROM user_admin  WHERE 1=1 AND user_name= '{$pData['username']}' AND user_pwd= '{$newPwd}' ";
+        	$ret = $this->mysqlQuery($sqltotal, "all");
+        	if(count($ret) === 1){
+        		return to_success($ret[0]);
+        	}else{
+        		return to_error('用户名或密码错误。');
+        	}
+        }else{
+        	return to_error('用户名或密码不合法。');
+        }
+        // return $res;
     }
 
 }
